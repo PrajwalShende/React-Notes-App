d680354572c9c9823ac3f0d4175995cde33a842d
PrajwalShende <prajwalshendeawsm@gmail.com>
2024-01-29 19:11:41 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NoteIcon from '@mui/icons-material/Note';
+
+function Header() {
+    return (<header>
+    <h1><NoteIcon/>Keeper</h1>
+    </header>
+    );
+}
+
+export default Header;
